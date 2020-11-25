e250db2ed3929922de7022024b1b25ac9c1baf32
CelinaJackson <celinanjackson@gmail.com>
2020-11-25 16:22:11 -0500

    made sighting_params

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -1,2 +1,8 @@
 class SightingsController < ApplicationController
+
+    private 
+
+    def sighting_params 
+        params.require(:sighting).permit(:title, :location, :description, :artist_name, :title_of_piece, :user_id, :museum_id, :artist_id, :piece_id)
+    end
 end 
