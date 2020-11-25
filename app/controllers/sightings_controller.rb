cbe39b6512daf8beb2b55d166ebac6955dca9d72
CelinaJackson <celinanjackson@gmail.com>
2020-11-25 17:24:33 -0500

    updating sighting params to reflect changes

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -3,6 +3,6 @@ class SightingsController < ApplicationController
     private 
 
     def sighting_params 
-        params.require(:sighting).permit(:title, :location, :description, :artist_name, :title_of_piece, :user_id, :museum_id, :artist_id, :piece_id)
+        params.require(:sighting).permit(:title, :museum, :description, :artist, :user_id)
     end
 end 
