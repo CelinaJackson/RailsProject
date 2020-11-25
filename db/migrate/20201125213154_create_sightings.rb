465ec4caa2d59133fb427e88c345ae89593e5aba
CelinaJackson <celinanjackson@gmail.com>
2020-11-25 17:22:23 -0500

    editing columns

--- db/migrate/20201125213154_create_sightings.rb
+++ db/migrate/20201125213154_create_sightings.rb
@@ -2,7 +2,8 @@ class CreateSightings < ActiveRecord::Migration[6.0]
   def change
     create_table :sightings do |t|
       t.string :title 
-      t.string :location
+      t.string :museum
+      t.string :artist
       t.string :description
       t.integer :user_id 
     end
